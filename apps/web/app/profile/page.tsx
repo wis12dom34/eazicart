eed52127aee99af216146696b75c249d4989054b
wis12dom34 <faceb8841@gmail.com>
2026-09-11 16:04:59 +0100

    style(web): format profile page

--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -67,10 +67,7 @@ export default function ProfilePage() {
             />
           </section>
           <section className="profile-menu">
-            <button
-              className="secondary-button"
-              onClick={() => auth.logout()}
-            >
+            <button className="secondary-button" onClick={() => auth.logout()}>
               Log out
             </button>
           </section>
